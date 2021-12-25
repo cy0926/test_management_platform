c8715096014c612b151ebbca0b5efbda8f2b48ce
cici <709695726@qq.com>
2021-12-25 19:45:31 +0800

    创建前端项目

---
+++ vue_interface_project/src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="Wellcome to Your vue.js App"/>
+
+    </div>
+</template>
+
+<script>
+    import HelloWorld from '../components/HelloWorld.vue'
+
+    export default {
+        name: 'home',
+        components: {
+            HelloWorld
+        },
+        props: {
+            arg: String
+        },
+        data(){
+            return{
+                message: 'message',
+                a: 'xxxx'
+            }
+        },
+        methods: {
+            getData(){
+
+            },
+        },
+        created(){
+
+        },
+    }
+</script>
